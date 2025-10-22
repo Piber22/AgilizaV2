95a00ec5ffb866013bed7683dd80c57bc62603fa
Piber22 <adrissonpiber22@gmail.com>
2025-10-21 21:38:55 -0300

    att2

--- adm/collect_rp.js
+++ adm/collect_rp.js
@@ -3,7 +3,7 @@
 // Extrair o ID da planilha da URL original
 // URL original: https://docs.google.com/spreadsheets/d/e/2PACX-1vRNW7MsqTP_FUxCN9AwLPH229I6M4ZeQqqb3STic9AMF_c2budXj0gKMFDRR-a4tW0JiMunFfcH9brR/pubhtml?gid=0
 const SHEET_ID = "2PACX-1vRNW7MsqTP_FUxCN9AwLPH229I6M4ZeQqqb3STic9AMF_c2budXj0gKMFDRR-a4tW0JiMunFfcH9brR";
-const GID = "1338979982"; // ID da aba "dados" (você precisa descobrir o GID correto)
+const GID = "1649158663"; // ID da aba "dados" (você precisa descobrir o GID correto)
 
 // URL para buscar como CSV (mais fácil de parsear)
 const sheetURL = `https://docs.google.com/spreadsheets/d/e/${SHEET_ID}/pub?gid=${GID}&single=true&output=csv`;
